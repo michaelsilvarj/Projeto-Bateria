e1c85566e80dfe0bf759c4efe6b8b5de7abe6b8b
Michael Silva <michaelsilv@gmail.com>
2022-04-11 22:47:05 -0300

    Configuração JS de evento de teclado no corpo HTML e função som

---
+++ script/script.js
@@ -0,0 +1,8 @@
+//Observa o HTML se haverá algum evento de teclado
+document.body.addEventListener('keyup',(event)=>{
+    playSound(event.code.toUpperCase());
+});
+
+function playSound(sound){
+
+}
